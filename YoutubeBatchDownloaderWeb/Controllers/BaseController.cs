8267a9c48f252619847e88d0aa9a19b684985997
gqy117 <gqy117@gmail.com>
2014-11-12 21:03:54 +0800

    [master] Encapsulated logic to BaseController.cs.

---
+++ YoutubeBatchDownloaderWeb/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿namespace YoutubeBatchDownloaderWeb.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public abstract class BaseController : Controller
+    {
+        protected const string DownloadFileTextPlain = "text/plain";
+
+        protected virtual FileStreamResult File(string fileContent,string fileDownloadName)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(fileContent);
+            var stream = new MemoryStream(byteArray);
+
+            return File(stream, DownloadFileTextPlain, fileDownloadName);
+        }
+    }
+}
--- YoutubeBatchDownloaderWeb/Controllers/YouTubeController.cs
+++ YoutubeBatchDownloaderWeb/Controllers/YouTubeController.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Web;
-using System.Web.Mvc;
-using YoutubeBatchDownloader.Service;
-
-namespace YoutubeBatchDownloaderWeb.Controllers
+﻿namespace YoutubeBatchDownloaderWeb.Controllers
 {
-    public class YouTubeController : Controller
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Web;
+    using System.Web.Mvc;
+    using YoutubeBatchDownloader.Service;
+
+    public class YouTubeController : BaseController
     {
         private readonly YoutubeTableHtmlParser YoutubeTableHtmlParser;
         private const string VbsFileName = "y.vbs";
+        private const string DownloadFileTextPlain = "text/plain";
 
         public YouTubeController()
         {
@@ -25,10 +26,7 @@ namespace YoutubeBatchDownloaderWeb.Controllers
         {
             string vbsString = YoutubeTableHtmlParser.Convert(youTubeHtml);
 
-            var byteArray = Encoding.ASCII.GetBytes(vbsString);
-            var stream = new MemoryStream(byteArray);
-
-            return File(stream, "text/plain", "y.vbs");   
+            return File(vbsString, VbsFileName);
         }
     }
 }
