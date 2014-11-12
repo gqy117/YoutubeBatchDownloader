d5ab923c8c198df72cb1fa63725ea05303acede2
gqy117 <gqy117@gmail.com>
2014-11-12 21:55:51 +0800

    [master] Re-factored ThunderVBSGenerator.cs.

--- YoutubeBatchDownloaderService/ThunderVBSGenerator.cs
+++ YoutubeBatchDownloaderService/ThunderVBSGenerator.cs
@@ -10,7 +10,9 @@
     public class ThunderVBSGenerator
     {
         private const string ThunderCreateObject = "Set ThunderAgent = CreateObject(\"ThunderAgent.Agent\")";
+        private const string ThunderAddTask = "Call ThunderAgent.AddTask(\"{0}\",\"{1}\",\"\",\"\",\"\",1,0,10)";
         private const string ThunderCommitTask = "Call ThunderAgent.CommitTasks()";
+
         private int StartPosition = 1;
         private bool DoesAddPrefix = true;
 
@@ -23,7 +25,7 @@
             {
                 for (int i = 0; i < input.Count; i++)
                 {
-                    result.AppendLine(String.Format("Call ThunderAgent.AddTask(\"{0}\",\"{1}\",\"\",\"\",\"\",1,0,10)", input[i].DownloadUrl, GetFileName(input[i].FileName, i)));
+                    result.AppendLine(String.Format(ThunderAddTask, input[i].DownloadUrl, GetFileName(input[i].FileName, i)));
                 }
             }
 
