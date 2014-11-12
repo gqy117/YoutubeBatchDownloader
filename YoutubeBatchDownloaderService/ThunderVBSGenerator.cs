c93a012d28153ab95cefc859e42b27b038fce419
gqy117 <gqy117@gmail.com>
2014-11-12 21:52:54 +0800

    [master] Added options to allow modify prefix.

--- YoutubeBatchDownloaderService/ThunderVBSGenerator.cs
+++ YoutubeBatchDownloaderService/ThunderVBSGenerator.cs
@@ -12,6 +12,7 @@
         private const string ThunderCreateObject = "Set ThunderAgent = CreateObject(\"ThunderAgent.Agent\")";
         private const string ThunderCommitTask = "Call ThunderAgent.CommitTasks()";
         private int StartPosition = 1;
+        private bool DoesAddPrefix = true;
 
         public string GenerateThunderVbs(IList<Video> input)
         {
@@ -22,13 +23,20 @@
             {
                 for (int i = 0; i < input.Count; i++)
                 {
-                    result.AppendLine(String.Format("Call ThunderAgent.AddTask(\"{0}\",\"{1}\",\"\",\"\",\"\",1,0,10)", input[i].DownloadUrl, (StartPosition + i).ToString("000") + input[i].FileName));
+                    result.AppendLine(String.Format("Call ThunderAgent.AddTask(\"{0}\",\"{1}\",\"\",\"\",\"\",1,0,10)", input[i].DownloadUrl, GetFileName(input[i].FileName, i)));
                 }
             }
 
             result.Append(ThunderCommitTask);
 
             return result.ToString();
+        }
+
+        private string GetFileName(string fileName, int i)
+        {
+            string prefix = DoesAddPrefix ? (StartPosition + i).ToString("000") : String.Empty;
+
+            return prefix + fileName;
         }
     }
 }
