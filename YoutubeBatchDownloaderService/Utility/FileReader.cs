e7855c4206b82e09ab2401c4fd7e1b847ceb3726
gqy117 <gqy117@gmail.com>
2014-11-14 22:01:00 +0800

    [master] Added FileReader.cs.

---
+++ YoutubeBatchDownloaderService/Utility/FileReader.cs
@@ -0,0 +1,20 @@
+﻿namespace YoutubeBatchDownloader.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class FileReader
+    {
+        public static string ReadString(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
