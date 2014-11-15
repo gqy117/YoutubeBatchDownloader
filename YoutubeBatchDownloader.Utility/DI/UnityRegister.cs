b4e6fff3134e0753a079842cff66974362395d8a
gqy117 <gqy117@gmail.com>
2014-11-15 14:26:34 +0800

    [master] StyleCop Utility.

--- YoutubeBatchDownloader.Utility/DI/UnityRegister.cs
+++ YoutubeBatchDownloader.Utility/DI/UnityRegister.cs
@@ -1,11 +1,11 @@
 ﻿namespace YoutubeBatchDownloader.Utility
 {
-    using Microsoft.Practices.Unity;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Microsoft.Practices.Unity;
 
     public class UnityRegister
     {
